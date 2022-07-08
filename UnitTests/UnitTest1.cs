9ff97e2f45e2d9887954e0432c0ffe67a1105ee3
C.J. Manca <cjmanca@hotmail.com>
2022-07-08 12:43:18 -0600

    Initial setup

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ plex-credit-detect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace plex_credit_detect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
