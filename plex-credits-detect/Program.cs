48d5a922e84e213992958cae49a95b9c5dfd0d2e
C.J. Manca <cjmanca@hotmail.com>
2022-07-09 03:24:14 -0600

    Initial Settings

---
+++ plex-credits-detect/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace plexCreditsDetect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Settings settings = new Settings();
+
+            settings.Load("C:\\test\\path\\to\\check\\");
+        }
+
+
+
+        public static void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ plex-credits-detect/settings.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Extensions.Configuration.Ini;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace plexCreditsDetect
+{
+    public class Settings : ICloneable
+    {
+        List<string> paths { get; set; } = new List<string>();
+
+        double detectionStart { get; set; } = 0.0;
+        double detectionEnd { get; set; } = 0.6;
+        int maximumSegments { get; set; } = 99;
+        int minimumFilesForMatch { get; set; } = 2;
+
+
+        object ICloneable.Clone()
+        {
+            Settings ret = new Settings();
+
+            ret.paths = paths.ToList();
+            ret.detectionStart = detectionStart;
+            ret.detectionEnd = detectionEnd;
+            ret.maximumSegments = maximumSegments;
+            ret.minimumFilesForMatch = minimumFilesForMatch;
+
+            return ret;
+        }
+
+
+
+
+        public void Load(string path = "")
+        {
+            string globalSettingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "plex-credits-detect");
+
+
+            if (!File.Exists(Path.Combine(globalSettingsPath, "fingerprint.ini")))
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "plex_credits_detect.resources.fingerprint.ini";
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+
+                    File.WriteAllText(Path.Combine(globalSettingsPath, "fingerprint.ini"), result);
+
+                    Console.WriteLine("Created default config file: " + Path.Combine(globalSettingsPath, "fingerprint.ini"));
+
+                    Program.Exit();
+                    return;
+                }
+            }
+
+            LoadSingle(globalSettingsPath);
+
+            List<string> pathParts = new List<string>();
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            while (di != null)
+            {
+                pathParts.Add(di.FullName);
+                di = di.Parent;
+            }
+
+            pathParts.Reverse();
+
+            foreach (var part in pathParts)
+            {
+                LoadSingle(part);
+            }
+
+        }
+
+
+        private void LoadSingle(string path)
+        {
+
+            if (!File.Exists(Path.Combine(path, "fingerprint.ini")))
+            {
+                return;
+            }
+
+            //string userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            IniConfigurationSource iniConfig = new IniConfigurationSource { Path = "fingerprint.ini", Optional = true, FileProvider = new PhysicalFileProvider(path) };
+            IniConfigurationProvider iniProvider = new IniConfigurationProvider(iniConfig);
+            iniProvider.Load();
+
+
+            string temp;
+            double dTemp;
+            int iTemp;
+
+
+            if (iniProvider.TryGet("default:detectionStart", out temp))
+            {
+                if (double.TryParse(temp, out dTemp))
+                {
+                    detectionStart = dTemp;
+                }
+            }
+            if (iniProvider.TryGet("default:detectionEnd", out temp))
+            {
+                if (double.TryParse(temp, out dTemp))
+                {
+                    detectionEnd = dTemp;
+                }
+            }
+
+            if (iniProvider.TryGet("default:maximumSegments", out temp))
+            {
+                if (int.TryParse(temp, out iTemp))
+                {
+                    maximumSegments = iTemp;
+                }
+            }
+
+            if (iniProvider.TryGet("default:minimumFilesForMatch", out temp))
+            {
+                if (int.TryParse(temp, out iTemp))
+                {
+                    minimumFilesForMatch = iTemp;
+                }
+            }
+
+
+            var dirs = iniProvider.GetChildKeys(new List<string>(),"directories");
+
+            foreach (var dir in dirs)
+            {
+                if (dir.Length > 0 && iniProvider.TryGet("directories:" + dir, out temp))
+                {
+                    paths.Add(temp);
+                }
+            }
+
+
+
+        }
+    }
+}
